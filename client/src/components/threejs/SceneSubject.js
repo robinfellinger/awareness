3d9fe8b6f6f2a1611c0075d1c33f04bc8b8e3a84
marie <dvorzak.marie@gmx.at>
2018-04-24 16:59:18 +0200

    basic anim bounce +  logic wor wakov random param

--- client/src/components/threejs/SceneSubject.js
+++ client/src/components/threejs/SceneSubject.js
@@ -2,9 +2,14 @@
 //// MODEL
 import floor from './floor.png';
 import * as THREE from 'three'
+var TWEEN = require('@tweenjs/tween.js');
+
 // import alphaTexture from '../../../../assets/textures/stripes_gradient.jpg';
 
 export default scene => {
+
+    var cRot = 0.0;
+    var tRot = 0.01;
     const group = new THREE.Group();
     const subjectGeometry = new THREE.SphereGeometry( 11, 32, 32 );
     const subjectMaterial = new THREE.MeshLambertMaterial({ color: "#6b6b6b", transparent: true, side: THREE.DoubleSide, alphaTest: 0.5 });
@@ -18,136 +23,51 @@ export default scene => {
     scene.add(group);
     drawFloor();
     group.rotation.z = Math.PI/4;
-
     const speed = 0.02;
     const textureOffsetSpeed = 0.02;
 
-    createSprings();
 
-
-
-
-
-    /**
-     * Creates a virtual spring between adjacent vertices in a
-     * face. Since vertices are shared between faces
-     * in the geometry, the faces are inherently connected to
-     * each other
-     */
-    function createSprings() {
-
-        var sphereFaces = subjectMesh.geometry.faces;
-
-        for(var f = 0; f < sphereFaces.length; f++) {
-            var face = sphereFaces[f];
-
-            if(face instanceof THREE.Face3) {
-
-                createSpring(face.a, face.b);
-                createSpring(face.b, face.c);
-                createSpring(face.c, face.a);
-
-            } else {
-
-                createSpring(face.a, face.b);
-                createSpring(face.b, face.c);
-                createSpring(face.c, face.d);
-                createSpring(face.d, face.a);
-
-            }
-        }
-    }
-
-    /**
-     * Displaces the vertices of a face
-     *
-     * @param {THREE.Face3|4} face The face to be displaced
-     * @param {Number} magnitude By how much the face should be displaced
-     */
-    function displaceFace(face, magnitude) {
-
-        // displace the first three vertices
-        displaceVertex(face.a, magnitude);
-        displaceVertex(face.b, magnitude);
-        displaceVertex(face.c, magnitude);
-
-        // if this is a face4 do the final one
-        if (face instanceof THREE.Face4) {
-            displaceVertex(face.d, magnitude);
-        }
-    }
-
-
-
-    /**
-     * Creates an individual spring
-     *
-     * @param {Number} start The index of the vertex for the spring's start
-     * @param {Number} end The index of the vertex for the spring's start
-     */
-    function createSpring(start, end) {
-        var sphereVertices = subjectMesh.geometry.vertices;
-        var startVertex    = sphereVertices[start];
-        var endVertex      = sphereVertices[end];
-
-        // if the springs array does not
-        // exist for a particular vertex
-        // create it
-        if(!startVertex.springs) {
-            startVertex.springs = [];
-
-            // take advantage of the one-time init
-            // and create some other useful vars
-            startVertex.normal = startVertex.clone().normalize();
-            startVertex.originalPosition = startVertex.clone();
-        }
-
-        // repeat the above for the end vertex
-        if(!endVertex.springs) {
-            endVertex.springs = [];
-            endVertex.normal = startVertex.clone().normalize();
-            endVertex.originalPosition = endVertex.clone();
-        }
-
-        if(!startVertex.velocity) {
-            startVertex.velocity = new THREE.Vector3();
-        }
-
-        // finally create a spring
-        startVertex.springs.push({
-
-            start   : startVertex,
-            end     : endVertex,
-            length  : startVertex.length(
-                endVertex
-            )
-
+    // group.position.set(10,10,0);
+    let position = { x : 0, y: 0 };
+    // let target = { x : 0, y: 2 };
+    let tween = new TWEEN.Tween(position)
+        .to({x: 0, y: -0.5, rotation: 0}, 3000)
+        .delay(900)
+        .easing(TWEEN.Easing.Elastic.Out)
+        .onUpdate(function(){
+            group.position.x = position.x;
+            group.position.y = position.y;
         });
+
+
+
+    let tweenBack = new TWEEN.Tween(position)
+        .to({x: 0, y: 0.5, rotation: 0}, 3000)
+        .delay(900)
+        .easing(TWEEN.Easing.Elastic.Out)
+        .onUpdate(function(){
+            group.position.x = position.x;
+            group.position.y = position.y;
+        });
+
+    tween.chain(tweenBack);
+    tweenBack.chain(tween);
+    tween.start();
+function wakov(){
+    if(Math.floor(Math.random()< 0.01)){
+        // if(tRot > 300){
+        //     tRot = 0;
+        // }else{
+        //     tRot += Math.random(0.1f, .25f);
+            tRot += Math.floor(Math.random() * .1) + .25;
+
+        // }
+    }else{
+
     }
-
-    /**
-     * Displaces an individual vertex
-     *
-     * @param {Number} vertex The index of the vertex in the geometry
-     * @param {Number} magnitude The degree of displacement
-     */
-    function displaceVertex(vertex, magnitude) {
-
-        var sphereVertices = subjectMesh.geometry.vertices;
-
-        // add to the velocity of the vertex in question
-        // but make sure we're doing so along the normal
-        // of the vertex, i.e. along the line from the
-        // sphere centre to the vertex
-        sphereVertices[vertex].velocity.add(
-
-            sphereVertices[vertex].normal.
-            clone().
-            multiplyScalar(magnitude)
-
-        );
-    }
-
+     cRot+=(tRot-cRot)/100000;
+    console.log(cRot);
+}
 
 
 
@@ -182,6 +102,7 @@ export default scene => {
     var particleSphere;
     var mesh2;
     var particleVertices;
+    // particles();
     // particles();
 // CREATE PARTICLES
     function particles(){
@@ -240,21 +161,36 @@ export default scene => {
 
 /////////////////////////////////////////////////////////////////////////////
 ////// UPDATE COMPONENT
+    Math.easeOutCubic = function (t, b, c, d) {
+        t /= d;
+        t--;
+        return c*(t*t*t + 1) + b;
+    };
+
     function update(time) {
+
         const angle = time*speed;
         group.rotation.y = angle;
         if (particleSphere) {
             particlemove();
         }
         const scale = (Math.sin(angle*8)+6.4)/5;
-        // updateVertexSprings();
-        //
-        // // flag that the sphere's geometry has
-        // // changed and recalculate the normals
-        // sphere.geometry.verticesNeedUpdate = true;
-        // sphere.geometry.normalsNeedUpdate = true;
-        // sphere.geometry.computeFaceNormals();
-        // sphere.geometry.computeVertexNormals();
+        subjectMesh.geometry.verticesNeedUpdate = true;
+        subjectMesh.geometry.normalsNeedUpdate = true;
+        // group.position.set(0, (tangle), 0);
+
+
+        // let tangleSPEED = 0;
+        // if(tangle <= -2 ){
+        //     tangle +=0.02;
+        //     tangleSPEED+=2;
+        // }else {
+        //     tangleSPEED =0;
+        //     tangle -=0.01;
+        // }
+        // tween.update();
+        wakov();
+       TWEEN.update();
 
     }
     return {
