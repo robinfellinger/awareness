7ddfa527e739de0dc33e94428ab90586031a7720
MartinaMaurer <s161023805@students.fh-hagenberg.at>
2018-04-26 13:17:27 +0200

    Storyworks!

--- client/src/components/interaction.js
+++ client/src/components/interaction.js
@@ -1,31 +1,48 @@
 import React, { Component } from 'react';
-import Message from "./message.js";
-import Answers from "./answer.js";
 import data from "./text.json";
 
+
 class Interaction extends Component {
+
     constructor(props){
         super(props);
-        this.state = {tag:props.tag};
+        this.state = {
+            IDTest: "Start",
+        }
+        this.updateID = this.updateID.bind(this);
     }
 
-    const question = data.passages.map(function(data){data.text});
-    const answers = data.passages.links.map(function(data){data.name});
+    updateID(id){
+        this.setState({IDTest: id})
+        //this.setState();
+    }
 
+// rowdata.name == start or rowdata.name == subrowdata.link
     render() {
+        //const question = data.passages.map(function(data){data});
         return (
-            <ul>
-                {
-                    data.passages.map(function (data) {
-                        if(data.name == this.state.tag) {
-                            return <li>{data.text}
-                                <button>test</button>
-                            </li>;
+            <div>{
+                data.passages.map((rowdata, i) =>
+                    <div className={"headline-lg"} key={rowdata.pid}>
+                        {rowdata.name === this.state.IDTest && rowdata.text}
+                        {
+                            (typeof(rowdata.links)=='object')?
+
+                            rowdata.links.map((subrowdata)=>
+
+                            <div key={subrowdata.pid*Math.random()}>
+                                {rowdata.name === this.state.IDTest &&
+                                <button  className={"button-basic text-sm t-transform-lowercase"} onClick={() => this.updateID(subrowdata.link)}>{subrowdata.name}</button>
+                                }
+                            </div>
+
+                            )
+                            :null
                         }
-                    })
-                }
-            </ul>
-        );
+
+                    </div>)
+            }</div>
+        )
     };
 }
 
