6a59353be1767a73334f3cfcb05e5dc64b3bb5da
marie <dvorzak.marie@gmx.at>
2018-05-01 14:18:22 +0200

    wisch-effekt

--- client/src/components/threejs/ModelLiquid.js
+++ client/src/components/threejs/ModelLiquid.js
@@ -14,7 +14,8 @@ var mesh = null;
 var warpVector = null;
 var warpVector2 = null;
 var cRot = 0.3;
-var tRot = 0.01;
+var tRot = 0.8;
+var mouseTimer = 0;
 var config = {
     frequenz: 1.4, //1.4 for wobble waves 0.2 for sublte rings 0.1 for extreme rings
     speed: 120, // + slower
@@ -72,6 +73,7 @@ class Model extends Component {
             color: new THREE.Color(0xE4E3F3),
             transparent: true,
             side: THREE.DoubleSide,
+            shininess: 70,
             alphaTest: 0.5
         });
 
@@ -85,7 +87,7 @@ class Model extends Component {
         mesh.rotation.y -= 200;
         this.context.scene.add(mesh);
 
-        warpVector = new THREE.Vector3(0, 50, 0);
+        warpVector = new THREE.Vector3(0, 50, 0); //50
         warpVector2 = new THREE.Vector3(20, 120, 0);
 
         console.log(warpVector);
@@ -102,9 +104,9 @@ class Model extends Component {
 
         function bounce(){
 
-            let position = { x : 0, y: 2 };
+            let position = { x : 0, y: 3 };
             let tween = new TWEEN.Tween(position)
-                .to({x: 0, y: -0.2, rotation: 0}, 2000)
+                .to({x: 0, y: -0.6, rotation: 0}, 2000)
                 .delay(1500)
                 .easing(TWEEN.Easing.Elastic.Out)
                 .onUpdate(function(){
@@ -120,8 +122,8 @@ class Model extends Component {
                     mesh.position.y = position.y;
                 });
 
-            tween.chain(tweenBack);
-            tweenBack.chain(tween);
+            tween.chain(tween);
+            // tweenBack.chain(tween);
             tween.start();
         }
     }
@@ -164,16 +166,17 @@ function wobble(){
         for (let i = 0; i < vertices.length; i++) {
             const v = vertices[i];
             const dist = v.distanceTo(warpVector);
-            const radian = (0.8 + 0.2 * Math.sin(dist / -frequenz + (time/speed))) * radius;
+            const radian = (0.8 + 0.2 * Math.sin(dist / -80 + (time/(100)))) * 80;
             v.normalize().multiplyScalar(radian);
         }
 
         mesh.geometry.verticesNeedUpdate = true;
 
-        const warpSine = (Math.sin(time/(speed * 8))) * (radius * 2);
+        const warpSine = (Math.sin(time/(speed * 8))) * (80 * 2);
         warpVector.y = warpSine;
         warpVector.x = warpSine;
         warpVector.z = warpSine;
+
 
         mesh.geometry.computeVertexNormals();
         mesh.geometry.computeFaceNormals();
@@ -182,6 +185,41 @@ function wobble(){
         mesh.geometry.normalsNeedUpdate = true;
         time++;
     }
+function mousemove() {
+    var mouseTolerance = 0.5;
+
+
+    document.onmousemove = function (e) {
+        var centerX = window.innerWidth * 0.5;
+        var centerY = window.innerHeight * 0.5;
+        // var centerZ = window.innerHeight * 0.5;
+        let mousePosX = (e.clientX - centerX) / centerX * mouseTolerance;
+        let mousePosY = (e.clientY - centerY) / centerY * mouseTolerance;
+
+        setTimeout(function () {
+            mesh.rotation.x -= 0.05 * (mousePosX * -1);
+            mesh.rotation.y += 0.05 * (mousePosY * -1);
+        }, 100);
+        if(mousePosX < 0 && mousePosY < 0){
+
+
+
+        }
+        if(mousePosX < 0 && mousePosY > 0){
+            console.log("links unten");
+            mouseTimer++;
+        }
+        if(mousePosX > 0 && mousePosY > 0){
+            console.log("rechts unten");
+            mouseTimer++;
+        }
+        if(mousePosX > 0 && mousePosY < 0){
+            console.log("rechts oben");
+            mouseTimer++;
+        }
+    };
+}
+mousemove();
 function wakov(){
       if(Math.floor(Math.random()< 0.01)){
                 if(tRot > 1.5){
@@ -204,7 +242,7 @@ function handleEmotions(emotion){
        wakov();//calculates random parameter for animations
        handleEmotions(this.props.emotion);
        wave();
-
+       //  wobble();
         function gameLoop(){
             TWEEN.update();
         }
