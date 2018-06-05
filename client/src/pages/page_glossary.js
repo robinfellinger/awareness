48686a4e99445ba2b17301ff88411ab04c4c47f9
MichiGarst <S1610238017@students.fh-hagenberg.at>
2018-06-05 16:04:41 +0200

    Glossar
    Glossar wird eingelesen

--- client/src/pages/page_glossary.js
+++ client/src/pages/page_glossary.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import data from "./glossar.json";
 
 
 
@@ -6,12 +7,31 @@ import React, { Component } from 'react';
 class Page_glossary extends Component {
 
 
+
     render(){
+        const test = data.name;
+        const yep = "binder";
+
         return(
             /* <SectionBlack title={"Title der section"} content={}>*/
             <section class="header ops-section test" data-index="1">
             <h2 class="header__titel">transgender <span class="header__titel--highlight">Glossar</span></h2>
             <a href="/" class="header__link">zurück</a>
+
+
+                <div>
+                    {
+                        data.glossar.map((dynamicT, i) =>
+                            <div>
+                                <b className="GlossarWord" >{dynamicT.word}: &nbsp;<br /></b>
+
+                                <span className="GlossarDescription" >{dynamicT.description}</span>
+                            </div>
+                        )
+
+
+                    }
+                </div>
             </section>
         );
     }
